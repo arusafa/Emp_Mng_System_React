426708de38daa765a78784beed5b051d8a6e372b
arusafa <arusafacollege@gmail.com>
2022-12-02 04:03:52 -0500

    delete button has changed

--- src/components/UpdateEmployee.js
+++ src/components/UpdateEmployee.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import React, {} from "react";
 import {Link} from "react-router-dom";
-import { useEffect } from "react";
+import React, {useEffect} from 'react';
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
