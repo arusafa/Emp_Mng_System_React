9e7a7eb05dfbeffa0f15f47779a3cf23c37cdd59
arusafa <arusafacollege@gmail.com>
2022-12-02 03:11:24 -0500

    delete button vindow.location.replace added

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -43,9 +43,8 @@ export default class EmployeeList extends Component {
                 })
 
                 this.setState({...this.state, employees: emp})
-                
-                window.location.reload();
             })
+            window.location.replace("https://comp3123-assignment-2.herokuapp.com/api/emp/employees")
         }
         
         render() {
