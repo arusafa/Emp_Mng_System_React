a8028d9d5b6f91911736a523352337ab715735a1
arusafa <99372405+arusafa@users.noreply.github.com>
2023-09-28 00:06:22 -0400

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -71,7 +71,7 @@ export default function SignUp() {
 
     const navigate = useNavigate();
     
-    const base_url = 'https://comp3123-assignment2-backend.herokuapp.com/api/user/signup';
+    const base_url = 'https://comp3123-assignment2-backend.onrender.com/api/user/signup';
     
     const [user, setUser] = useState({
         username: '',
